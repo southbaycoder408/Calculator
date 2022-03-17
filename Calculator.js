3c47cb02c1b079cb91057124e4bf9acfcb49791b
southbaycoder408 <jedheadgaming@gmail.com>
2022-03-16 22:08:10 -0700

    added add,subtrat,multiple, division AND operate functions

---
+++ Calculator.js
@@ -0,0 +1,24 @@
+let sum = (num1,num2) => num1 + num2;
+
+let difference = (num1, num2) => num1-num2;
+
+let product = (num1, num2) => num1*num2;
+
+let quotient = (num1, num2) => num1/num2;
+
+let operate = (operator, num1,num2) =>{
+    if(operator == "+" ){
+        return sum(num1,num2);
+    }
+    else if(operator == "-"){
+        return difference(num1,num2);
+    }
+    else if(operator == "*"){
+        return product(num1, num2)
+    }
+    else if(operator == "/"){
+        return quotient(num1,num2);
+    }
+
+}
+
