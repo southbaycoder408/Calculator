cbf5af55ecb6a68ec753233187b6ae4407465641
southbaycoder408 <jedheadgaming@gmail.com>
2022-03-18 21:59:39 -0700

    added buttons with labels for each calculator function including numbers and operators

--- components.js
+++ components.js
@@ -1,10 +1,25 @@
 //creates div container that will hold all visible components of calculator
-const calculatorContainer = document.createElement('div');
+const calculatorBody = document.createElement('div');
 //adds div to body tag
-document.body.appendChild(calculatorContainer);
+document.body.appendChild(calculatorBody);
 //adds class 'container' to div
-calculatorContainer.classList.add('container');
+calculatorBody.classList.add('container');
 
-const button = document.createElement('button');
-button.textContent ="PRESS ME";
-document.body.appendChild(button);
+//creates button that will be number/operator keys for calculator
+// const button = document.createElement('button');
+// //adds text into the button
+// button.textContent ="PRESS ME";
+// //appends button to calulator container div
+// calculatorBody.appendChild(button);
+
+const button2 = () => {
+    const buttonNames = ["0","1","2","3","4","5","6","7","8","9","A/C","Enter","+","-","x","/"] 
+    let buttons = 0;
+    for(buttons = 0; buttons <= 15; buttons++){
+        const button = document.createElement('button');
+        button.textContent = buttonNames[buttons];
+        calculatorBody.appendChild(button);
+    }
+}
+
+button2();
