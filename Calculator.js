c96dc839225fe4cbe4afa25bcebbb34a6addbd7e
southbaycoder408 <jedheadgaming@gmail.com>
2022-03-20 23:07:24 -0700

    added some code trying to use operating functions

--- Calculator.js
+++ Calculator.js
@@ -21,3 +21,4 @@ let operate = (operator, num1,num2) =>{
     }
 }
 
+
--- components.js
+++ components.js
@@ -1,12 +1,13 @@
 //declare a variable that will store the computed value of calculator
 let calcValue = "" ;
+let calcNum1 = "";
+let calcNum2 = "";
+let calcTotal = "";
 
 const calcDisplay = document.createElement('div');
 document.body.appendChild(calcDisplay);
 calcDisplay.setAttribute('id', 'display');
 calcDisplay.textContent = "0";
-//document.getElementById("display").innerHTML = calcValue;
-
 
 //creates div container that will hold all visible components of calculator
 const calcBody = document.createElement('div');
@@ -18,9 +19,9 @@ calcBody.classList.add('calcBody');
 //function creates all the standard calcultor buttons
 const createButtons = () => {
     //array with preset labels for each button
-    const buttonNames = ["0","1", "2", "3", "4", "5", "6", "7", "8", "9", "A/C", "Enter", "+", "-", "x", "/", "Clear"]
+    const buttonNames = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A/C", "Enter", "+", "-", "x", "/"]
     let buttons = 0;
-    for (buttons = 0; buttons <= 16; buttons++) {    
+    for (buttons = 0; buttons <= 15; buttons++) {    
         //creates button that will be number/operator keys for calculator
         const button = document.createElement('button');
         //adds text into the button
@@ -31,14 +32,41 @@ const createButtons = () => {
 
         //creates event listener that that returns a value based on button click
         button.addEventListener("click",() => {
-            calcValue += buttonValue;
-            console.log(calcValue);
-            document.getElementById("display").innerHTML = calcValue;
+            
+            if(buttonValue === "+"){
+                calcNum1 += calcValue;
+                document.getElementById("display").innerHTML = " + ";
+                console.log("calcNum1: " + calcNum1);
+            }
+            else if(buttonValue === "-"){
+                
+            }
+            else if(buttonValue === "x"){
+                
+            }
+            else if(buttonValue === "/"){
+                
+            }
+            else if(buttonValue === "A/C"){
+                
+            }
+            else if(buttonValue === "Enter"){
+                calcNum2 += calcValue;
+                calcTotal = calcNum1 + calcNum2 ;
+                document.getElementById("display").innerHTML = calcTotal;
+                
+            }
+            else{
+                calcValue += buttonValue;
+                console.log("calcValue: " + calcValue);
+                document.getElementById("display").innerHTML = calcValue;
+            }
+            
+            
         } )
 
         //appends button to calulator container div
         calcBody.appendChild(button);
     }
 }
-
 createButtons();
