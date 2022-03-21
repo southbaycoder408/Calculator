e30b93faddfb39c8e7e8ae8dadf23b476b257a0f
southbaycoder408 <jedheadgaming@gmail.com>
2022-03-20 22:15:41 -0700

    added event listener to button which detects button clicks and also will update the display value at the same time.

--- components.js
+++ components.js
@@ -1,6 +1,12 @@
+//declare a variable that will store the computed value of calculator
+let calcValue = "" ;
+
 const calcDisplay = document.createElement('div');
 document.body.appendChild(calcDisplay);
-calcDisplay.textContent = 'CALCULATOR DISPLAY';
+calcDisplay.setAttribute('id', 'display');
+calcDisplay.textContent = "0";
+//document.getElementById("display").innerHTML = calcValue;
+
 
 //creates div container that will hold all visible components of calculator
 const calcBody = document.createElement('div');
@@ -9,19 +15,27 @@ document.body.appendChild(calcBody);
 //adds class 'container' to div
 calcBody.classList.add('calcBody');
 
-
-
-
-
+//function creates all the standard calcultor buttons
 const createButtons = () => {
     //array with preset labels for each button
-    const buttonNames = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A/C", "Enter", "+", "-", "x", "/", "Clear"]
+    const buttonNames = ["0","1", "2", "3", "4", "5", "6", "7", "8", "9", "A/C", "Enter", "+", "-", "x", "/", "Clear"]
     let buttons = 0;
-    for (buttons = 0; buttons <= 16; buttons++) {
+    for (buttons = 0; buttons <= 16; buttons++) {    
         //creates button that will be number/operator keys for calculator
         const button = document.createElement('button');
         //adds text into the button
         button.textContent = buttonNames[buttons];
+
+        //creates value that will be applied  to each button
+        const buttonValue = buttonNames[buttons];
+
+        //creates event listener that that returns a value based on button click
+        button.addEventListener("click",() => {
+            calcValue += buttonValue;
+            console.log(calcValue);
+            document.getElementById("display").innerHTML = calcValue;
+        } )
+
         //appends button to calulator container div
         calcBody.appendChild(button);
     }
