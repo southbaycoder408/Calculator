a672cb8bebfdf6b571a4a2f7b520b645e92a7f05
southbaycoder408 <jedheadgaming@gmail.com>
2022-03-26 00:21:46 -0700

    added the html elements buttons, and cleaned up JS

--- components.js
+++ components.js
@@ -1,11 +1,7 @@
 let sum = (num1, num2) => num1 + num2;
-
 let difference = (num1, num2) => num1 - num2;
-
 let product = (num1, num2) => num1 * num2;
-
 let quotient = (num1, num2) => num1 / num2;
-
 let operate = (operator, num1, num2) => {
     if (operator == "+") {
         return sum(num1, num2);
@@ -17,11 +13,9 @@ let operate = (operator, num1, num2) => {
         return quotient(num1, num2);
     }
 };
-
-let isOperator = (input) =>{
+let isOperator = (input) => {
     return (input == "+" || input == "-" || input == "/" || input == "x");
-  }
-
+}
 //declare a variable that will store the computed value of calculator
 let displayVal = "";
 let calcNum1 = "";
@@ -30,42 +24,26 @@ let subTotal = "";
 let calcTotal = "";
 let operator = "";
 
-const calcDisplay = document.createElement('div');
-document.body.appendChild(calcDisplay);
-calcDisplay.setAttribute('id', 'display');
-calcDisplay.textContent = "";
+//function creates all the standard calculator buttons
+const attachButtonEvents = () => {
 
-//creates div container that will hold all visible components of calculator
-const calcBody = document.createElement('div');
-//adds div to body tag
-document.body.appendChild(calcBody);
-//adds class 'container' to div
-calcBody.classList.add('calcBody');
-
-//function creates all the standard calcultor buttons
-const createButtons = () => {
-    //array with preset labels for each button
-    const buttonNames = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A/C", "Enter", "+", "-", "x", "/"]
-    let buttons = 0;
-    for (buttons = 0; buttons <= 15; buttons++) {    
-        
-        
-        //creates button that will be number/operator keys for calculator
-        const button = document.createElement('button');
-        //adds text into the button
-        button.textContent = buttonNames[buttons];
-
+    //Used query selector to get all buttons in DOM
+    const buttons = document.querySelectorAll("button");
+    //console.log(buttons);
+    buttons.forEach((currentButton) => {
         //creates value that will be applied  to each button
-        const buttonValue = buttonNames[buttons];
+        
+        let buttonText = currentButton.textContent;
 
+        const buttonValue = buttonText;
 
         //creates event listener function that that returns a value based on button click
-        button.addEventListener("click",() => {
+        currentButton.addEventListener("click", () => {
 
-            if(isOperator(buttonValue)){
-                
+            if (isOperator(buttonValue)) {
+
                 operator += buttonValue;
-                
+
                 //assigns the display value to subTotal variable
                 subTotal += calcNum1;
                 //resets the display value and calcNum1 value
@@ -74,30 +52,25 @@ const createButtons = () => {
 
                 document.getElementById("display").innerHTML = " + ";
                 console.log("subTotal: " + subTotal);
-            }
-                
-            else if(buttonValue === "A/C"){
-                
-            }
-            else if(buttonValue === "Enter"){
+            } else if (buttonValue === "A/C") {
+
+            } else if (buttonValue === "Enter") {
 
                 calcNum2 += calcNum1;
-                let calcTotal = operate(operator,Number(calcNum1),Number(calcNum2));
-                //console.log("operator: "+ operator+" calcNum1: "+calcNum1 +" calcNum2: "+calcNum2 + " calcTotal: "+ calcTotal);
+                let calcTotal = operate(operator, Number(calcNum1), Number(calcNum2));
+                console.log("operator: " + operator + " calcNum1: " + calcNum1 + " calcNum2: " + calcNum2 + " calcTotal: " + calcTotal);
                 document.getElementById("display").innerHTML = calcTotal;
-                
-            }
-            else{
+
+            } else {
                 displayVal += buttonValue;
                 calcNum1 += buttonValue;
                 console.log("Display Val: " +displayVal + " and calcNum1: "+ calcNum1);
                 document.getElementById("display").innerHTML = displayVal;
             }
-        } )
+        })
 
-        //appends button to calulator container div
-        calcBody.appendChild(button);
-    }
+    })
+
 }
 
-createButtons();
+attachButtonEvents();
