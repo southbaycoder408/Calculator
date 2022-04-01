60d77675c769180e633fe5fbad11baa3010ed707
southbaycoder408 <jedheadgaming@gmail.com>
2022-04-01 00:01:45 -0700

    added code to quotient to display broken when dividing by 0

--- components.js
+++ components.js
@@ -3,7 +3,14 @@
 const sum = (num1, num2) => num1 + num2;
 const difference = (num1, num2) => num1 - num2;
 const product = (num1, num2) => num1 * num2;
-const quotient = (num1, num2) => num1 / num2;
+const quotient = (num1, num2) => {
+    if(num2 == 0){
+        return "broken.";
+    } else {
+        return num1 / num2;
+    }
+}
+
 const operate = (operator, num1, num2) => {
     if (operator == "+") {
         return sum(num1, num2);
